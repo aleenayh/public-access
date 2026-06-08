164b6fe53d0920d2e5a9d3c2c846c33c382ef019
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-06-08 08:37:12 -0700

    just some formatting

--- src/components/shared/AnimatedHeader.tsx
+++ src/components/shared/AnimatedHeader.tsx
@@ -5,10 +5,10 @@ import { usePreferences } from "../../context/PreferencesContext";
 export function AnimatedHeader({text} : {text: string}) {
     const {theme} = usePreferences();
 	const title = useMemo(() => {
-		return 		text.split("").map((letter, index) => (
- <span data-letter={letter} className="animatedHeaderLetter" key={index}>{letter}</span>
+		return text.split("").map((letter, index) => (
+          <span data-letter={letter} className="animatedHeaderLetter" key={index}>{letter}</span>
 		));
-		//force entry animations on theme change
+	//force entry animations on theme change
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [theme])
 
