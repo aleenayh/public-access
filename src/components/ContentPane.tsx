067d7a0cfe712dc71774b95b6a13427660dce360
aleenayh <145587682+aleenayh@users.noreply.github.com>
2026-06-11 15:55:37 -0700

    footer fixes

--- src/components/ContentPane.tsx
+++ src/components/ContentPane.tsx
@@ -4,10 +4,9 @@ import { MysteryPane } from "./MysteryPane";
 
     export function ContentPane() {
 	return (
-		<StyledPane variant="vertical">
-			<h3>Content Pane</h3>
-            <MysteryPane/>
-			<FooterNav/>
-		</StyledPane>
+			<StyledPane variant="vertical">
+				<MysteryPane />
+                <FooterNav/>
+            </StyledPane>
 	);
 }
